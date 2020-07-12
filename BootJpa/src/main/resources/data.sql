16d772d02b495cb94e7f1632ecc93c066b0cc190
itsupport <itsupport@192.168.0.2>
2020-07-12 18:56:24 -0400

    commit

--- BootJpa/src/main/resources/data.sql
+++ BootJpa/src/main/resources/data.sql
@@ -4,3 +4,5 @@ insert into Alien(id,name,lang)   values(103,'Advika','java');
 insert into Alien(id,name,lang)  values(104,'Samyukta','unix');
 insert into Alien(id,name,lang)   values(105,'Yagnik','php');
 insert into Alien(id,name,lang)   values(106,'Padma','unix');
+insert into Alien(id,name,lang)   values(106,'Padma1','unix');
+insert into Alien(id,name,lang)   values(106,'Padma2','unix');
