959aebd5809d001c938c45cde5c8ae482326b55a
srivarshampt <68202677+srivarshampt@users.noreply.github.com>
2020-07-13 19:24:41 -0400

    Update TestStaticMethod.java

--- simple/src/com/abc/def/TestStaticMethod.java
+++ simple/src/com/abc/def/TestStaticMethod.java
@@ -40,9 +40,9 @@ public class TestStaticMethod{
 public static void main(String args[]){
 Student.change();//calling change method
 //creating objects
-Student s1 = new Student(111,"Karan12");
-Student s2 = new Student(222,"Aryan12");
-Student s3 = new Student(333,"Sonoo12");
+Student s1 = new Student(111,"Karan13");
+Student s2 = new Student(222,"Aryan13");
+Student s3 = new Student(333,"Sonoo13");
 //calling display method1 
 //calling display method1
 //calling display method
